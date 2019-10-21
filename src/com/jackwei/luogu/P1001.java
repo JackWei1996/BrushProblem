662d57f75161ee6866b776db60c7ecead189b0f9
Jack魏 <1960486253@qq.com>
2019-10-21 14:22:10 +0800

    洛谷P1001

---
+++ src/com/jackwei/luogu/P1001.java
@@ -0,0 +1,28 @@
+package com.jackwei.luogu;
+
+import java.util.Scanner;
+
+/**
+ * 作者：Jack魏
+ *
+ * 题目描述
+ * 输入两个整数a,b，输出它们的和(|a|,|b|<=10^9)。
+ *
+ * 输入格式
+ * 两个整数以空格分开
+ *
+ * 输出格式
+ * 一个数
+ *
+ * 输入输出样例
+ * 输入            输出
+ * 20 30           50
+ */
+public class P1001 {
+    public static void main(String[] args) {
+        Scanner cin=new Scanner(System.in);
+        long a = cin.nextLong();
+        long b = cin.nextLong();
+        System.out.println(a + b);
+    }
+}
